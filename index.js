ffea45d079f2396b5b10b2c7e02c161775777e0c
mariobarrosmbn <166873448+mariobarrosmbn@users.noreply.github.com>
2024-07-26 14:35:35 -0300

    add UI
    stylesheet created
    updated logic - DOM manipulation
    updated HTML

--- index.js
+++ index.js
@@ -1,84 +1,78 @@
-// THE ROCK-PAPER-SCISSORS GAME - JAVASCRIPT - THE ODIN PROJECT
+// THE ROCK-PAPER-SCISSORS GAME - UI - THE ODIN PROJECT
 
-// DEFINIR AS JOGADAS:
-//  - JOGADA DO USUÁRIO;
-//  - JOGADA DO COMPUTADOR;
-// COMPARAR AS JOGADAS E DEFINIR O VENCEDOR
-// ANOTAR O PLACAR ATÉ ATINGIR A MELHOR DE CINCO
-// OFERECER OPÇÃO PARA REINÍCIO DO JOGO
-// =======================================================================
+// CREATE 3 BUTTONS, ONE FOR EACH SELECTION (done in the HTML file);
 
-// VARIÁVEIS GLOBAIS:
+
+// ADD DIV FOR DISPLAYING RESULTS AND USE DOM METHODS INSTEAD OF CONSOLE.LOG;
+// UPDATE THE SCORE UNTIL ONE PLAYER REACHES FIVE WINS;
+
 
 
 // Computer's move:
 
-function getComputerChoice () {
-    let cpu = Math.ceil(Math.random() * 3);
-    if (cpu === 1) {
-        return 'ROCK';
-    } else if (cpu === 2) {
-        return 'PAPER';
-    } else if (cpu === 3) {
-        return 'SCISSORS';
+function getComputerChoice() {
+    let cpuPlay = Math.ceil(Math.random() * 3);
+    if (cpuPlay === 1) {
+        cpu = 'ROCK';
+    } else if (cpuPlay === 2) {
+        cpu = 'PAPER';
+    } else if (cpuPlay === 3) {
+        cpu = 'SCISSORS';
     }
 }
-
-// user's move:
-
-function getHumanChoice () {
-    return prompt('ROCK, PAPER OR SCISSORS? ').toUpperCase();
-}
-
-// console.log(getHumanChoice());
-// console.log(computerChoice);
-// playRound(humanChoice, computerChoice);
 
 // playing a round:
 
-function playRound(user, cpu) {
-    if (user === cpu) {
-        console.log(`Draw! ${user} equals to ${cpu}.`);
-    } else if (user === 'ROCK' && cpu === 'SCISSORS') {
-        humanScore += 1;
-        console.log(`You win! ${user} beats ${cpu}.`);
-    } else if (user === 'PAPER' && cpu === 'ROCK') {
-        humanScore += 1;
-        console.log(`You win! ${user} beats ${cpu}.`);
-    } else if (user === 'SCISSORS' && cpu === 'PAPER') {
-        humanScore += 1;
-        console.log(`You win! ${user} beats ${cpu}.`);
-    } else if (user === 'ROCK' && cpu === 'PAPER'){
-        computerScore += 1;
-        console.log(`You lose! ${cpu} beats ${user}.`);
-    } else if (user === 'PAPER' && cpu === 'SCISSORS') {
-        computerScore += 1;
-        console.log(`You lose! ${cpu} beats ${user}.`);
-    } else if (user === 'SCISSORS' && cpu === 'ROCK') {
-        computerScore += 1;
-        console.log(`You lose! ${cpu} beats ${user}.`);
-    }
-}
-
-// playing a game of five rounds:
-
 let humanScore = 0;
 let computerScore = 0;
+let results = document.querySelector('.results');
+let score = document.querySelector('.score');
 
-function playGame () {
-    for (i = 0; i < 5; i++) {
-        // getComputerChoice();
-        // getHumanChoice();   
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    if (humanScore === computerScore) {
-        console.log(`FINAL SCORE: ${humanScore} x ${computerScore}. DRAW!`);
-    } else if (humanScore > computerScore) {
-        console.log(`FINAL SCORE: ${humanScore} x ${computerScore}. YOU WIN!`);
-    } else if (humanScore < computerScore) {
-        console.log(`FINAL SCORE: ${humanScore} x ${computerScore}. YOU LOSE!`);
+function playRound(user, cpu) {
+    if (user === cpu) {
+        results.textContent = `DRAW! ${user} equals to ${cpu}.`;     
+    } else if (user === 'ROCK' && cpu === 'SCISSORS') {
+        humanScore += 1;
+        results.textContent = `You win! ${user} beats ${cpu}.`;
+    } else if (user === 'PAPER' && cpu === 'ROCK') {
+        humanScore += 1;
+        results.textContent = `You win! ${user} beats ${cpu}.`;
+    } else if (user === 'SCISSORS' && cpu === 'PAPER') {
+        humanScore += 1;
+        results.textContent = `You win! ${user} beats ${cpu}.`;
+    } else if (user === 'ROCK' && cpu === 'PAPER'){
+        computerScore += 1;
+        results.textContent = `You lose! ${cpu} beats ${user}.`;
+    } else if (user === 'PAPER' && cpu === 'SCISSORS') {
+        computerScore += 1;
+        results.textContent = `You lose! ${cpu} beats ${user}.`;
+    } else if (user === 'SCISSORS' && cpu === 'ROCK') {
+        computerScore += 1;
+        results.textContent = `You lose! ${cpu} beats ${user}.`;
     }
 }
-playGame();
+
+let user = '';
+let cpu = '';
+
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', () => {
+    user = 'ROCK';
+    getComputerChoice();
+    playRound(user, cpu);
+});
+
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', () => {
+    user = 'PAPER';
+    getComputerChoice();
+    playRound(user, cpu);
+});
+
+const scissors = document.querySelector('.scissors');
+scissors.addEventListener('click', () => {
+    user = 'SCISSORS';
+    getComputerChoice();
+    playRound(user, cpu);
+});
+
